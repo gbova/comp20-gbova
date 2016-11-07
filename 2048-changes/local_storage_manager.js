ceec35ae75d95c3ad20783a0022164c77d46502e
Gabriella Bova <bova3.0@gmail.com>
2016-11-06 22:41:49 -0500

    Add changes to original 2048 game

---
+++ 2048-changes/local_storage_manager.js
@@ -0,0 +1,94 @@
+window.fakeStorage = {
+  _data: {},
+
+  setItem: function (id, val) {
+    return this._data[id] = String(val);
+  },
+
+  getItem: function (id) {
+    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+  },
+
+  removeItem: function (id) {
+    return delete this._data[id];
+  },
+
+  clear: function () {
+    return this._data = {};
+  }
+};
+
+function LocalStorageManager() {
+  this.bestScoreKey     = "bestScore";
+  this.gameStateKey     = "gameState";
+
+  var supported = this.localStorageSupported();
+  this.storage = supported ? window.localStorage : window.fakeStorage;
+}
+
+LocalStorageManager.prototype.localStorageSupported = function () {
+  var testKey = "test";
+  var storage = window.localStorage;
+
+  try {
+    storage.setItem(testKey, "1");
+    storage.removeItem(testKey);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+// Best score getters/setters
+LocalStorageManager.prototype.getBestScore = function () {
+  return this.storage.getItem(this.bestScoreKey) || 0;
+};
+
+LocalStorageManager.prototype.setBestScore = function (score) {
+  this.storage.setItem(this.bestScoreKey, score);
+};
+
+// Game state getters/setters and clearing
+LocalStorageManager.prototype.getGameState = function () {
+  var stateJSON = this.storage.getItem(this.gameStateKey);
+  return stateJSON ? JSON.parse(stateJSON) : null;
+};
+
+LocalStorageManager.prototype.setGameState = function (gameState) {
+  this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
+};
+
+
+
+
+/*
+ * Modified by Gabriella Bova
+ *
+ * Makes a POST request to save game score before deleting data locally
+ */
+var request = new XMLHttpRequest();
+LocalStorageManager.prototype.clearGameState = function () {
+  // Load the entire gameState from localStorage:
+  gameState = this.storage.getItem(this.gameStateKey);
+  if (gameState == null) {    // Game was already cleared
+    return;
+  }
+  gameState = JSON.parse(gameState);
+  gameGrid = gameState["grid"];
+  gameScore = gameState["score"];
+  gameUser = "Gabriella";   // TODO: Change this and prompt user for username
+
+  // Make an HTTP POST request to the server:
+  var url = "https://radiant-lake-20317.herokuapp.com/submit.json";
+  var params = {
+    username: gameUser,
+    score: gameScore,
+    grid: gameGrid
+  }
+  params = JSON.stringify(params);
+  request.open("post", url, true);
+  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  request.send(params);
+
+  this.storage.removeItem(this.gameStateKey);
+};
